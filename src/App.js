b7e84a0fa0c4ab9c3743c36fee24504b0d34b024
patrickbatungbacal <patrickbatungbacal@gmail.com>
2021-01-20 14:26:07 +0800

    5. Styling with Styled Components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 //import pages
 import AboutUs from "./pages/AboutUs";
 
+//import global style
+import GlobalStyle from "./components/global style/GlobalStyle";
+
 function App() {
   return (
     <div className="App">
+      <GlobalStyle />
       <AboutUs />
     </div>
   );
