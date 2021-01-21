37914027c89a8c2f0a17e858b6e264a6ce411977
patrickbatungbacal <patrickbatungbacal@gmail.com>
2021-01-21 15:31:47 +0800

    16. Animate OurWork Contents

--- src/animation.js
+++ src/animation.js
@@ -14,7 +14,6 @@ export const pageAnimation = {
   },
   exit: {
     opacity: 0,
-    y: 300,
     transition: {
       duration: 0.5,
     },
@@ -47,7 +46,29 @@ export const photoAnim = {
     opacity: 1,
     transition: {
       ease: "easeOut",
-      duration: 0.75,
+      duration: 0.5,
     },
+  },
+};
+
+export const lineAnim = {
+  hidden: { width: "0%" },
+  show: { width: "100%", transition: { duration: 1 } },
+};
+
+export const slider = {
+  hidden: { x: "-130%", skew: "45deg" },
+  show: {
+    x: "100%",
+    skew: "0deg",
+    transition: { ease: "easeOut", duration: 1 },
+  },
+};
+
+export const sliderContainer = {
+  hidden: { opacity: 1 },
+  show: {
+    opacity: 1,
+    transition: { ease: "easeOut", staggerChildren: 0.12 },
   },
 };
